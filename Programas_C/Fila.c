3a53a56c14f57fe909e403004b53a4c06fd05293
RyanG.R <148453862+Goncalvesrocha@users.noreply.github.com>
2026-03-31 23:58:10 -0300

    Fix logic for checking full and empty queue

--- Programas_C/Fila.c
+++ Programas_C/Fila.c
@@ -9,140 +9,133 @@ int retirar_fila(int* filaG);
 int is_empty(int* filaG);
 int is_full(int* filaG);
 
-
-int xG, iG = 0; // A letra G vem de Variável Global
-
+int xG, iG = 0;
 
 int is_full(int* filaG){
 
    int i = 0;
-   int cont = 0;
 
-  while(i < 4){
+   while(i < 4){
+      if (filaG[i] == 0){ 
+         iG = i;
+         return 1; 
+      }
+      i++;
+   }
 
-     if (filaG[i] == NULL){
-         iG = i; // Guarda o índice que está vazio
-         return 1; //Não está cheio
-     }
-         i++;
-  }
-
+   return 0; 
 }
-
 
 int is_empty(int* filaG){
 
- int i = 0;
- int cont = 0;
+   int i = 0;
 
-  while(i < 4){
+   while(i < 4){
+      if (filaG[i] != 0){ 
+         iG = i;
+         return 1; 
+      }
+      i++;
+   }
 
-     if (filaG[i] != NULL){
-         iG = i; //Guarda o índice que tem um valor
-         return 1; //Não está vázio
-     }
-        i++;
-  }
+   return 0; 
 }
-
-
-
 
 int inserir_fila(int* filaG) {
 
    if(is_full(filaG) == 1){
 
-      printf("A Fila NÃO Está CHEIA\n*Inserindo na fila* :");
+      printf("A Fila NÃƒO EstÃ¡ CHEIA\n*Inserindo na fila* :");
       scanf("%d", &xG);
-      filaG[iG] = xG; // O indíce que está vázio recebe um novo valor
+      filaG[iG] = xG;
 
       exibir_fila(filaG);
 
+   } else {
+      printf("A Fila EstÃ¡ CHEIA\nNÃƒO Ã© possÃ­vel inserir!\n\n");
+      return 1; 
    }
-   else
-      printf("A Fila Está CHEIA\nNÃO é possível inserir!\n\n");
-      return 1;
 
-  return 0;
+   return 0;
 }
-
 
 int retirar_fila(int* filaG){
 
-    if(is_empty(filaG) == 1){
+   if(is_empty(filaG) == 1){
 
-        printf("\nA Fila NÃO Está Vázia\nRetirando...\n");
-        filaG[iG] = NULL;
+      printf("\nA Fila NÃƒO EstÃ¡ Vazia\nRetirando...\n");
+      filaG[iG] = 0; 
 
-        int o = 0;
-        while(o < 4){
+      int o = 0;
+      while(o < 4){
 
-              if(filaG[o] == 0){
+         if(filaG[o] == 0){
 
-                 for(int i = o + 1; i < 4; i++){
-
-                     if(filaG[i] != 0){
-                        filaG[o] = filaG[i];
-                        filaG[i] = 0;
-                        break;
+            for(int i = o + 1; i < 4; i++){
+               if(filaG[i] != 0){
+                  filaG[o] = filaG[i];
+                  filaG[i] = 0;
+                  break;
+               }
             }
-       }
+         }
+         o++;
+      }
+
+      exibir_fila(filaG);
+
+   } else {
+      printf("\nNÃ£o HÃ¡ Nenhum NÃºmero Na Fila\n\n");
+      return 1; 
    }
-   o++;
-}
-        exibir_fila(filaG);
 
-    }
-    else
-       printf("\nNão Há Nenhum Número Na Fila\n\n");
-       return 1;
-
-  return 0;
+   return 0;
 }
 
 void exibir_fila(int* filaG){
 
-    int  i = 0;
+   int i = 0;
 
-    do{
-        if(filaG[i] != NULL)
-            printf("%d|", filaG[i]);
+   do{
+      if(filaG[i] != 0) 
+         printf("%d|", filaG[i]);
 
-           i++;
-    }
-    while(i < 4);
-    puts("");
+      i++;
+   }
+   while(i < 4);
+
+   puts("");
 }
 
 int main(){
 
-    setlocale(LC_ALL, "Portuguese");
-    int filaG[4] = {};
-    int yG, iG;
+   setlocale(LC_ALL, "Portuguese");
 
-    inserir_fila(filaG);
-    inserir_fila(filaG);
-    inserir_fila(filaG);
-    inserir_fila(filaG);
+   int filaG[4] = {0}; 
+   int yG, iG;
 
-    retirar_fila(filaG);
-    retirar_fila(filaG);
-    inserir_fila(filaG);
-    inserir_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
 
-    inserir_fila(filaG);
-    inserir_fila(filaG);
-    retirar_fila(filaG);
-    retirar_fila(filaG);
+   retirar_fila(filaG);
+   retirar_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
 
-    inserir_fila(filaG);
-    retirar_fila(filaG);
-    retirar_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
+   retirar_fila(filaG);
+   retirar_fila(filaG);
 
-    inserir_fila(filaG);
-    inserir_fila(filaG);
-    inserir_fila(filaG);
+   inserir_fila(filaG);
+   retirar_fila(filaG);
+   retirar_fila(filaG);
 
-    return 0;
+   inserir_fila(filaG);
+   inserir_fila(filaG);
+   inserir_fila(filaG);
+
+   return 0;
 }
-
